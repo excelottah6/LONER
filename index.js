f42e470d9944a44fab210cb08102060d71e0f9ef
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-09-27 09:53:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,49 +1,41 @@
 const fs = require('fs');
 const { Client } = require('whatsapp-web.js');
+const qrcode = require('qrcode-terminal');
 
-const SESSION_FILE_PATH = './session.json'; // Change this to your session file path
+const SESSION_FILE_PATH = './session/session.json'; // Change this path as needed
 
 let sessionData;
 
-// Load session data if available
+// Load session data if it exists
 if (fs.existsSync(SESSION_FILE_PATH)) {
   sessionData = require(SESSION_FILE_PATH);
 }
 
 const client = new Client({
   session: sessionData,
-  puppeteer: {
-    args: ['--no-sandbox'],
-  },
 });
 
-client.on('qr', (qrCode) => {
-  console.log('Scan the QR code below to authenticate:');
-  console.log(qrCode);
+client.on('qr', (qr) => {
+  console.log('Scan the QR code to log in:');
+  qrcode.generate(qr, { small: true });
 });
 
 client.on('authenticated', (session) => {
-  console.log('Authenticated!');
+  console.log('Authenticated as ' + session.user.name);
   // Save the session data to a file
   fs.writeFileSync(SESSION_FILE_PATH, JSON.stringify(session));
 });
 
 client.on('ready', () => {
-  console.log('WhatsApp bot is ready!');
+  console.log('Client is ready!');
 });
 
+client.initialize();
+
+// Handle your bot logic here
 client.on('message', (message) => {
-  // Handle incoming messages here
+  // Handle incoming messages
+  console.log('Received message:', message.body);
 });
 
-client.initialize();
-
-// Handle disconnects
-client.on('disconnected', (reason) => {
-  console.log('Disconnected:', reason);
-  // You can add code here to automatically reconnect if desired
-});
-
-// Start the bot
-client.initialize();
-
+// Handle other events and bot logic as needed
