c96130f43c94852340a221ca1ea60a39ac0073f4
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-11-26 20:06:09 +0100

    Create script.js

---
+++ public/script.js
@@ -0,0 +1,7 @@
+function redirectToBotRepo() {
+  window.location.href = 'https://github.com/excelottah6/IZUKU-MD';
+}
+
+function showQRCode() {
+  window.location.href = 'https://replit.com/@Excelottah/BLACK-Md');
+}
