775f1680aba53aff22d0fd9bf52c7fb7d91fb80a
EXCEL <119881740+excelottah6@users.noreply.github.com>
2023-11-27 10:01:55 +0100

    Update script.js

--- public/script.js
+++ public/script.js
@@ -1,7 +1,7 @@
-function redirectToBotRepo() {
-  window.location.href = 'https://github.com/excelottah6/IZUKU-MD';
+function showBotRepo() {
+  window.location.href = 'https://link-to-your-bot-repo'; // Replace with your actual bot repo link
 }
 
 function showQRCode() {
-  window.location.href = 'https://replit.com/@Excelottah/BLACK-Md');
+  window.location.href = 'https://link-to-your-qr-code'; // Replace with your actual QR code link
 }
