952e3a01160067de51a338418e8d178d941dffff
EXCEL <119881740+excelottah6@users.noreply.github.com>
2024-01-27 22:58:46 +0100

    Delete public/script.js

--- public/script.js
+++
@@ -1,7 +0,0 @@
-function showBotRepo() {
-  window.location.href = 'https://link-to-your-bot-repo'; // Replace with your actual bot repo link
-}
-
-function showQRCode() {
-  window.location.href = 'https://link-to-your-qr-code'; // Replace with your actual QR code link
-}
