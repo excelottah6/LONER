5c07dbf678fd79d81dde7e29e9fe3de6259d6e34
EXCEL <119881740+excelottah6@users.noreply.github.com>
2024-03-29 22:31:33 +0100

    Update server.js

--- server.js
+++ server.js
@@ -4,11 +4,20 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Redirect root route to "index.html"
 app.get('/', (req, res) => {
   res.redirect('/index.html');
 });
+
+// Route to serve pair.html
+app.get('/pair', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'pair.html'));
+});
+
+// Start the server
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
 });
